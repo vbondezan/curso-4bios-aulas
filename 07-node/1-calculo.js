4163667b0febcf4a83520a53cd4676385cedd87c
vbondezan <vbondezan@hotmail.com>
2024-01-08 11:28:24 -0300

    Rename 07-node/1_calculo.js to 07-node/1-calculo.js

---
+++ 07-node/1-calculo.js
@@ -0,0 +1,7 @@
+let num1 = 25;
+let res = num1 ** 2;
+res = Math.pow(res,2);
+let saida1 = "O resultado de num1: "+num1+" ao quadrado ao quadrado é "+res;
+let saida2 = `O resultado de num1: ${num1} ao quadrado ao quadrado é ${res}`;
+console.log(saida1)
+console.log(saida2)
